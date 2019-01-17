7f21cc8f1111ce1322a986030e358d08ac2f97a4
LiunanChen <liunan_chen@163.com>
2019-01-17 19:40:54 +0800

    change mysql config for dev

--- db/databaseConfig.js
+++ db/databaseConfig.js
@@ -1,24 +1,35 @@
 let config = null;
-if (process.env.NODE_ENV == 'dev') {
-  config = {
-    host: '192.168.1.38',
-    port: 3306,
-    user: 'root',
-    password: '123456',
-    database: 'mysite',
-    multipleStatements: true,
-    timezone: '08:00' // 配置时区
-  };
-} else if (process.env.NODE_ENV == 'prod') {
-  config = {
-    host: '173.82.212.76',
-    port: 3306,
-    user: 'admin',
-    password: 'admin',
-    database: 'mysite',
-    multipleStatements: true,
-    timezone: '08:00' // 配置时区
-  };
+
+// 服务器mysql配置
+const serverConfig = {
+  host: '173.82.212.76',
+  port: 3306,
+  user: 'admin',
+  password: 'admin',
+  database: 'mysite',
+  multipleStatements: true,
+  timezone: '08:00' // 配置时区
+};
+
+// 本地mysql配置
+const localConfig = {
+  host: '192.168.1.38',
+  port: 3306,
+  user: 'root',
+  password: '123456',
+  database: 'mysite',
+  multipleStatements: true,
+  timezone: '08:00' // 配置时区
+};
+
+switch (process.env.NODE_ENV) {
+  case 'prod':
+    config = serverConfig;
+    break;
+
+  default:
+    config = localConfig;
+    break;
 }
 
 module.exports = {
