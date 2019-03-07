499812bd4ae8057fd75ccdc74e7693c1861f44c5
LiunanChen <liunan_chen@163.com>
2019-03-07 18:53:07 +0800

    add typeorm

--- models/User.ts
+++ models/User.ts
@@ -1,9 +1,18 @@
-export class User {
-    name: string;
-    constructor(name: string) {
-        this.name = name;
-    }
-    sayName(): void {
-        console.log(this.name)
-    }
+import {BaseEntity, Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    isActive: boolean;
+
 }
